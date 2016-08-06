eb977ee7ce5443e3c9e62a70ed159c8cd8d8154a
emilyperegrine <emilyperegrine@EMILYWIN81PARRA>
2016-08-06 13:53:38 +0100

    Completed basic project structure

--- Script_Sanitize_CSharp/ScriptSantizer.cs
+++ Script_Sanitize_CSharp/ScriptSantizer.cs
@@ -8,5 +8,9 @@ namespace Script_Sanitize_CSharp
 {
     public static class ScriptSantizer
     {
+        public static string Sanitize(string html)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
